3ebf90fbc7ae01350432887aca31d4231c115a4d
reya <reya@lume.nu>
2023-11-18 17:55:57 +0700

    fix for tauri v2

--- tauri-plugin-theme/src/platform/macos.rs
+++ tauri-plugin-theme/src/platform/macos.rs
@@ -7,7 +7,10 @@ use tauri::{command, AppHandle, Manager, Runtime};
 
 #[command]
 pub fn set_theme<R: Runtime>(app: AppHandle<R>, theme: Theme) -> Result<(), &'static str> {
-    save_theme_value(&app.config(), theme);
+    save_theme_value(
+        &app.path().config_dir().expect("failed to get config dir"),
+        theme,
+    );
     for window in app.windows().values() {
         let ptr = window.ns_window().map_err(|_| "Invalid window handle")?;
         unsafe {
