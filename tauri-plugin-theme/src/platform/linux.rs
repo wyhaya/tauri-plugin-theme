1b8bab206915329f40e0382eb752bcbdbc2e43f0
reya <reya@lume.nu>
2023-11-20 08:46:17 +0700

    fix

--- tauri-plugin-theme/src/platform/linux.rs
+++ tauri-plugin-theme/src/platform/linux.rs
@@ -12,7 +12,7 @@ use tokio::sync::{
 
 #[command]
 pub fn set_theme<R: Runtime>(app: AppHandle<R>, theme: Theme) -> Result<(), &'static str> {
-    save_theme_value(app, theme);
+    save_theme_value(&app, theme);
     match theme {
         Theme::Auto => {
             async_runtime::spawn(start_proxy(app));
--- tauri-plugin-theme/src/platform/macos.rs
+++ tauri-plugin-theme/src/platform/macos.rs
@@ -7,7 +7,7 @@ use tauri::{command, AppHandle, Manager, Runtime};
 
 #[command]
 pub fn set_theme<R: Runtime>(app: AppHandle<R>, theme: Theme) -> Result<(), &'static str> {
-    save_theme_value(app.clone(), theme);
+    save_theme_value(&app, theme);
     for window in app.windows().values() {
         let ptr = window.ns_window().map_err(|_| "Invalid window handle")?;
         unsafe {
