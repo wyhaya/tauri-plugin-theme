c8f5d54a2c367dd8f2351cabd5bfb661a3ec512e
reya <reya@lume.nu>
2024-02-04 09:09:10 +0700

    feat: upgrade to tauri v2 beta

--- tauri-plugin-theme/src/platform/macos.rs
+++ tauri-plugin-theme/src/platform/macos.rs
@@ -8,7 +8,7 @@ use tauri::{command, AppHandle, Manager, Runtime};
 #[command]
 pub fn set_theme<R: Runtime>(app: AppHandle<R>, theme: Theme) -> Result<(), &'static str> {
     save_theme_value(&app, theme);
-    for window in app.windows().values() {
+    for window in app.webview_windows().values() {
         let ptr = window.ns_window().map_err(|_| "Invalid window handle")?;
         unsafe {
             let val = match theme {
